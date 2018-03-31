6064fc80c27f29c8cce28803d23251e254ccb59e
RYCKEMBUSCH Anthony <anthony.ryck@hotmail.fr>
2018-03-31 17:31:49 +0200

    Mise à jour test unitaire, pour rremettre dans l'état les données.

--- Zeus.Test/Codes/ShowsAndMoviesManagerTest.cs
+++ Zeus.Test/Codes/ShowsAndMoviesManagerTest.cs
@@ -11,26 +11,75 @@ namespace Zeus.Test.Codes
     [TestClass]
     public class ShowsAndMoviesManagerTest
     {
+        private const string PATH_MOVIE = "/movies";
+
+
+        [TestCleanup]
+        public void TestInitialize()
+        {
+            // Suppression des fichiers de tests (movies, shows,...)
+            DirectoryInfo directoryMovie = new DirectoryInfo(PATH_MOVIE);
+            foreach (FileInfo file in directoryMovie.GetFiles())
+            {
+                file.Delete();
+            }
+
+            // Suppression des fichiers de configuration créé.
+            DirectoryInfo directoryApp = new DirectoryInfo(AppContext.BaseDirectory + "/save");
+            directoryApp.Delete(true);
+
+            DirectoryInfo directoryConfig = new DirectoryInfo(AppContext.BaseDirectory + "/config");
+            directoryConfig.Delete(true);
+        }
+
+
+
+
+        [TestMethod]
+        public void RecuperationDeFilms_When_MovieFolderIsEmpty_Then_NoFilm()
+        {
+
+            #region Act
+
+            ShowsAndMoviesManager manager = new ShowsAndMoviesManager();
+            var result = manager.GetListMoviesLocal();
+
+            #endregion
+
+            #region Assert
+
+            Assert.AreEqual(0, result.Count(), "Doit contenir aucun film.");
+
+            #endregion
+        }
+
+
         [TestMethod]
         public void RecuperationDeFilms_When_MovieFolderHas2Films_Then_2Films()
         {
             #region Arrange
 
-            string pathMovie = "/movies";
+            
 
-            if (!Directory.Exists(pathMovie))
+            if (!Directory.Exists(PATH_MOVIE))
             {
-                Directory.CreateDirectory(pathMovie);
+                Directory.CreateDirectory(PATH_MOVIE);
             }
 
             // Création de faux films.
-            DirectoryInfo directoryMovie = new DirectoryInfo(pathMovie);
+            DirectoryInfo directoryMovie = new DirectoryInfo(PATH_MOVIE);
 
-            File.Create(directoryMovie.FullName + @"\An.American.Haunting.2005.TRUEFRENCH.DVDRip.XviD.Mp3-Tetine.avi");
-            File.Create(directoryMovie.FullName + @"\paddington-2-french-bluray-1080p-2018.mkv");
-            File.Create(directoryMovie.FullName + @"\mazinger-z-french-bluray-1080p-2018.mp4");
-            File.Create(directoryMovie.FullName + @"\fichierWeb.html");
-            File.Create(directoryMovie.FullName + @"\readme.txt");
+            var fsAmercian = File.Create(directoryMovie.FullName + @"\An.American.Haunting.2005.TRUEFRENCH.DVDRip.XviD.Mp3-Tetine.avi");
+            var movie2 = File.Create(directoryMovie.FullName + @"\paddington-2-french-bluray-1080p-2018.mkv");
+            var movie3 = File.Create(directoryMovie.FullName + @"\mazinger-z-french-bluray-1080p-2018.mp4");
+            var other1 = File.Create(directoryMovie.FullName + @"\fichierWeb.html");
+            var other2 = File.Create(directoryMovie.FullName + @"\readme.txt");
+
+            fsAmercian.Dispose();
+            movie2.Dispose();
+            movie3.Dispose();
+            other2.Dispose();
+            other1.Dispose();
 
             #endregion
 
@@ -47,5 +96,7 @@ namespace Zeus.Test.Codes
 
             #endregion
         }
+
+
     }
 }
