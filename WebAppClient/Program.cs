5185211dac6cd3a53ee1efb2f24f25a78def084a
RYCKEMBUSCH Anthony <anthony.ryck@hotmail.fr>
2018-04-15 14:25:43 +0200

    Changement sur le css

--- WebAppClient/Program.cs
+++ WebAppClient/Program.cs
@@ -14,39 +14,22 @@ namespace WebAppClient
 {
     public class Program
     {
-#if ServiceOnly
-        #region ServiceOnly
-        public static void Main(string[] args)
-        {
-            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
 
-            var host = WebHost.CreateDefaultBuilder(args)
-                .UseContentRoot(pathToContentRoot)
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.RunAsService();
-        }
-        #endregion
-#endif
-//#if ServiceOrConsole
         #region ServiceOrConsole
         public static void Main(string[] args)
         {
-            bool isService = true;
-            if (Debugger.IsAttached || args.Contains("--console"))
-            {
-                isService = false;
-            }
+            //bool isService = true;
+            //if (Debugger.IsAttached || args.Contains("--console"))
+            //{
+            //    isService = false;
+            //}
 
             var pathToContentRoot = Directory.GetCurrentDirectory();
-            if (isService)
-            {
-                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-                pathToContentRoot = Path.GetDirectoryName(pathToExe);
-            }
+            //if (isService)
+            //{
+            //    var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            //    pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            //}
 
             var host = WebHost.CreateDefaultBuilder(args)
                 .UseContentRoot(pathToContentRoot)
@@ -54,50 +37,16 @@ namespace WebAppClient
                 .UseApplicationInsights()
                 .Build();
 
-            if (isService)
-            {
-                host.RunAsService();
-            }
-            else
-            {
+            //if (isService)
+            //{
+            //    host.RunAsService();
+            //}
+            //else
+            //{
                 host.Run();
-            }
+            //}
         }
         #endregion
-//#endif
-#if HandleStopStart
-        #region HandleStopStart
-        public static void Main(string[] args)
-        {
-            bool isService = true;
-            if (Debugger.IsAttached || args.Contains("--console"))
-            {
-                isService = false;
-            }
 
-            var pathToContentRoot = Directory.GetCurrentDirectory();
-            if (isService)
-            {
-                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-                pathToContentRoot = Path.GetDirectoryName(pathToExe);
-            }
-
-            var host = WebHost.CreateDefaultBuilder(args)
-                .UseContentRoot(pathToContentRoot)
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            if (isService)
-            {
-                host.RunAsCustomService();
-            }
-            else
-            {
-                host.Run();
-            }
-        }
-        #endregion
-#endif
     }
 }
